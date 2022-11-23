2ca8e0c51f44f0f432f4d27326fec59d9a7aca7e
chaerlo127 <monynony0203@gmail.com>
2022-11-24 00:15:04 +0900

    feat: room db 생성

---
+++ ari_chae_eun/week8_umc/app/src/main/java/com/example/week8_umc/UserDao.kt
@@ -0,0 +1,27 @@
+package com.example.week8_umc
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+    @Insert
+    fun insert(user:User)
+
+    @Delete
+    fun delete(user:User)
+
+    @Query("SELECT * FROM User")
+    fun selectAll() : List<User>
+
+    @Query("SELECT * FROM User WHERE userId = :userId")
+    fun selectByUserId(userId: User) : User
+
+    @Query("SELECT * FROM User WHERE name = :name")
+    fun selectByUserName(name: String) : List<User>
+
+    @Query("UPDATE User SET name = :name WHERE userId = :userId")
+    fun updateByUserId (userId: Int, name: String) // 업데이트 할 변수를 대부분 뒤에다가 넣어준다.
+}
